235e6d442774b124530781e0086446cf9cd27077
Saloom <116690661+isayiwantacost@users.noreply.github.com>
2023-03-12 16:05:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const ln1 = ('./constants.ts')
+const ln2 = ('./tsconfig.ts')
+const lnlist = [...new Set(ln1, ln2)];
+
 var http = require('http');
 http.createServer(function(req,res){
     res.write("Your own personal HTTP server. You may decorate this.");
