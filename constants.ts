1084609f3aebf827d9cdd822bc0765349bf347e0
Saloom <116690661+isayiwantacost@users.noreply.github.com>
2023-03-12 16:01:27 -0500

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,4 @@
+let server: string = "localhost";
+const json = JSON.parse("localhost:8080");
+console.log(server, json);
+console.log("If this appears in the console of your browser, the server has ran successfully.");
