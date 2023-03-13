126478c3e884e59c4908c8a3d552f27a5c351869
KENNY77 <116690661+isayiwantacost@users.noreply.github.com>
2023-03-13 16:03:16 -0500

    Update and rename index.js to index8080.js

--- index8080.js
+++ index8080.js
@@ -1,7 +1,3 @@
-const ln1 = ('./constants.ts')
-const ln2 = ('./tsconfig.ts')
-const lnlist = [...new Set(ln1, ln2)];
-
 var http = require('http');
 http.createServer(function(req,res){
     res.write("Your own personal HTTP server. You may decorate this.");
