3355891a675dda0878c9502c4da88b5e88024bcb
Anvil <116690661+isayiwantacost@users.noreply.github.com>
2023-06-10 22:49:52 -0500

    Delete constants.ts

--- constants.ts
+++
@@ -1,5 +0,0 @@
-/* This is for pure decoration. This is added for no reason LOL */
-let server: string = "localhost";
-const json = JSON.parse("localhost:8080");
-console.log(server, json);
-console.log("If this appears in the console of your browser, the server has ran successfully.");
